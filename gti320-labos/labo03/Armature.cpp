9284d81adc3e88cd97c6b3cb98e18e4c2a8cac5d
HH22OO <nicolas.riopel.1@ens.etsmtl.ca>
2023-12-08 10:48:42 -0500

    etape 1 et 2

--- gti320-labos/labo03/Armature.cpp
+++ gti320-labos/labo03/Armature.cpp
@@ -18,17 +18,36 @@ void Link::forward()
 {
     // TODO Create a rotation matrix from the Euler angles
     //      of the current link.
+    Matrix3f rot = makeRotation(eulerAng(0), eulerAng(1), eulerAng(2));    
+
     
     // TODO Create a local 4D rigid transformation matrix from the 
     //      3D rotation matrix and translation of the current link.
+    Matrix4f transRig;
+    transRig.setIdentity();
+    transRig.block(0, 0, 3, 3) = rot;
+    transRig(0, 3) = trans(0);
+    transRig(1, 3) = trans(1);
+    transRig(2, 3) = trans(2);
 
     // TODO Update the global transformation for the link using the
     //      parent's rigid transformation matrix and the local transformation.
     //      Hint : the parent matrix should be post-multiplied.
     //      Hint : the root does not have a parent. You must consider this case.
+    //  NOUVELLE FONCTION
+    if ((*this).isRoot()) {
+        M = transRig;
+    }
+    else {
+        M = (*parent).M * transRig;
+    }
 
     // TODO Update the transformation of child links
     // by recursion.
+    int nbEnfants = (*this).enfants.size();
+    for (int i = 0; i < nbEnfants; i++) {
+        (*this).enfants[i]->forward();
+    }
 
 }
 
@@ -56,7 +75,21 @@ void Armature::pack(Vector<float, Dynamic>& _theta)
 {
     // TODO Collect the Euler angles of each link and put them
     //      into the dense vector @a theta
+    
+    // nb de links 
+    int nbLinks = links.size();
 
+    // ajustement de la taille de theta
+    _theta.resize(nbLinks * 3);
+
+    // Pour chaque links dans l'armature
+    for (int i = 0; i < nbLinks; i++) {
+        // ref au link
+        Link* linkRef = links[i];
+        _theta(i * 3) = (*linkRef).eulerAng(0);
+        _theta(i * 3 + 1) = (*linkRef).eulerAng(1);
+        _theta(i * 3 + 2) = (*linkRef).eulerAng(2);
+    }
 }
 
 void Armature::unpack(const Vector<float, Dynamic>& _theta)
@@ -69,4 +102,12 @@ void Armature::unpack(const Vector<float, Dynamic>& _theta)
     //      for each link in the armature.
     // 
 
+    // Pour chaque links dans l'armature
+    for (int i = 0; i < links.size(); i++) {
+        // ref au link
+        Link* linkRef = links[i];
+        (*linkRef).eulerAng(0) = _theta(i * 3);
+        (*linkRef).eulerAng(1) = _theta(i * 3 + 1);
+        (*linkRef).eulerAng(2) = _theta(i * 3 + 2);
+    }
 }
